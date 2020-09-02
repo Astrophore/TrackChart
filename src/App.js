cab3efba545f6b5b70ee154cd7bfc0c6ef117fda
Seow Wei Jie <seowwei.jie@ad.infosys.com>
2020-09-03 02:48:46 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'Stock Ticker Chart',
+      items:[],
+      currentItem:{
+        ticker:'',
+        price:'',
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        [e.target.text]: e.target.value
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h2>{this.state.title}</h2>
+      <form id="stockForm" className="stockForm" onSubmit={this.addItem}>
+        <input type="text" placeholder="Enter symbol/ticker" value={this.state.currentItem.ticker} onChange={this.handleInput}/>
+        <input type="text" placeholder="Enter indicative price bought" value={this.state.currentItem.price} onChange={this.handleInput}/>
+        <button onClick={(e)=>this.formSubmit(e)}> Add</button>
+      </form>
+      </div>
+    );
+  }
 }
 
 export default App;
+
