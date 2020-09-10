7eac803a6508610f5299b7552e6549a3b00dc889
Seow Wei Jie <seowwei.jie@ad.infosys.com>
2020-09-10 18:05:22 +0800

    routing for home page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router";
+import { StockForm } from "./components/StockForm";
 
-class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state={
-      title: 'Stock Ticker Chart',
-      items:[],
-      currentItem:{
-        ticker:'',
-        price:'',
-      }
-    }
-    this.handleInput = this.handleInput.bind(this);
-    this.addItem = this.addItem.bind(this);
-  }
-  handleInput(e){
-    this.setState({
-      currentItem:{
-        [e.target.text]: e.target.value
-      }
-    })
-  }
-  addItem(e){
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    console.log(newItem);
-  }
-  render() {
+function App() {
   return (
-    <div className="App">
-      <h2>{this.state.title}</h2>
-      <form id="stockForm" className="stockForm" onSubmit={this.addItem}>
-        <input type="text" placeholder="Enter symbol/ticker" value={this.state.currentItem.ticker} onChange={this.handleInput}/>
-        <input type="text" placeholder="Enter indicative price bought" value={this.state.currentItem.price} onChange={this.handleInput}/>
-        <button onClick={(e)=>this.formSubmit(e)}> Add</button>
-      </form>
-      </div>
-    );
-  }
+    <div style={{ maxWidth: "30rem", margin: "4rem auto" }}>
+      <Router>
+        <Switch>
+          <Route exact path="/" Component={StockForm} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
-
